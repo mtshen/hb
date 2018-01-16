a3920d8cffc9b59dc66e61dfcb1462233c1b952b
mtshen <mtshen@37degree.com>
2018-01-16 14:54:17 +0800

    https

--- option.js
+++ option.js
@@ -5,5 +5,5 @@ Think.opt('path', path.join(Think.DIR, 'www'));
 Think.opt('user_path', path.join(Think.DIR, 'user'));
 Think.opt('log_path', path.join(Think.DIR, 'log'));
 Think.opt('https_switch', true);
-Think.opt('https_pfx', '../https/server.pfx');
-Think.opt('https_pass', 'shen1234QWER');
+Think.opt('https_key', '../https/server-key.pem');
+Think.opt('https_cert', '../https/server-cert.pem');
