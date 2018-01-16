bfd6dc0d9b1117139afad9eee2259400e46926f1
mtshen <mtshen@37degree.com>
2018-01-16 14:41:47 +0800

    a

--- option.js
+++ option.js
@@ -1,5 +1,6 @@
 // 配置文件
 let path = require('path');
+Think.opt('port', '8637');
 Think.opt('path', path.join(Think.DIR, 'www'));
 Think.opt('user_path', path.join(Think.DIR, 'user'));
 Think.opt('log_path', path.join(Think.DIR, 'log'));
