83f8601b375619efc40133a83e623c6353479b1c
mtshen <mtshen@37degree.com>
2018-01-19 11:29:07 +0800

    sql

--- user/getuser_data.js
+++ user/getuser_data.js
@@ -1,13 +1,42 @@
+const operation = require('./mysql/operation');
+
+/**
+ * 查询用户数据
+ * 先查询用户数据, 如果用户没有该数据则新增一个空数据, 并返回
+ */
 Think.answer({
     url: '/getuser_data',
     callback: (data, {response}) => {
         const openid = data.openid;
         // 测试数据
-        const SQLDATA = {id: openid, balance: 1, send: 0, receive: 1, data: ''};
-        response.writeHead(200, {"Content-Type": 'application/json; charset=utf-8'});
-        response.end(JSON.stringify(SQLDATA));        
+        operation.query('user', {id: openid}, (error, dataList) => {
+            if (error) {
+                return errorHandle(error, response);
+            }
+
+            const SQLDATA = {id: openid, balance: 0, send: 0, receive: 0, data: ''};
+            if (dataList.length) {
+                console.log(JSON.stringify(dataList[0]));
+                response.writeHead(200, {"Content-Type": 'application/json; charset=utf-8'});
+                response.end(JSON.stringify(dataList[0]));
+            } else {
+                operation.add('user', SQLDATA, (error) => {
+                    if (error) {
+                        return errorHandle(error, response);
+                    }
+                    response.writeHead(200, {"Content-Type": 'application/json; charset=utf-8'});
+                    response.end(JSON.stringify(SQLDATA));
+                });
+            }
+        });
+    
         return Think.END;
     }
 });
 
-
+function errorHandle(error, response) {
+    console.log('[ERROR: /getuser_data]', error.message);
+    response.writeHead(200, {"Content-Type": 'application/json; charset=utf-8'});
+    response.end(JSON.stringify({error: true, message: error.message}));
+    return false;
+}
---
+++ user/mysql/operation.js
@@ -0,0 +1,88 @@
+// 操作sql测试
+const mysql = require('mysql');
+const option = require('./option.json');
+const sql = new Mysql(option);
+
+class Mysql {
+    constructor(option) {
+        super();
+        this.connection = mysql.createConnection(option);
+        this.swit = false; // true：开启, false: 关闭
+    }
+
+    // 连接sql
+    connect() {
+        const { swit } = this;
+        if (!swit) {
+            this.connection.connect();
+            this.swit = true;
+        }
+    }
+
+    // 结束连接
+    end() {
+        const { swit } = this;
+        if (swit) {
+            this.connection.end();
+            this.swit = false;
+        }
+    }
+
+    // mysql 数据增
+    add(tableName, tableData, callback = this.invalidCallback) {
+        this.connect();
+        let tableDataList = [];
+        let tableKeyList = [];
+
+        for (let tableKey in tableData) {
+            tableDataList.push(tableData[tableKey]);
+            tableKeyList.push(tableKey);
+        }
+
+        this.connection.query(
+            `INSERT INTO ${tableName}(${tableKeyList.join()}) VALUES(${tableDataList.join()})`,
+            callback
+        );
+    }
+
+    // mysql 数据删
+    remove(tableName, condition, callback = this.invalidCallback) {
+        this.connect();
+        let conditionText = [];
+        for (let key in condition) {
+            conditionText.push(`${key}=${condition[key]}`);
+        }
+
+        connection.query(`DELETE FROM ${tableName} where ${conditionText.join()}`, callback);
+    }
+
+    // mysql 数据改 表名/修改数据/修改条件/回调函数
+    update(tableName, tableData = {}, condition = {}, callback = this.invalidCallback) {
+        this.connect();
+        let tableDataText = [];
+        let conditionText = [];
+        for (let key in tableData) {
+            tableDataText.push(`${key}=${tableData[key]}`);
+        }
+
+        for (let key in condition) {
+            conditionText.push(`${key}=${condition[key]}`);
+        }
+
+        connection.query(`UPDATE ${tableName} SET ${tableDataText.join()} WHERE ${conditionText.join()}`, callback);
+    }
+
+    // mysql 数据查
+    query(tableName, callback = this.invalidCallback) {
+        this.connect();
+        this.connection.query(`SELECT * FROM ${tableName}`, callback);
+    }
+
+    invalidCallback(error) {
+        if (error) {
+            console.log('[MYSQL ERROR]', error.message);
+        }
+    }
+}
+
+module.exports = sql;
